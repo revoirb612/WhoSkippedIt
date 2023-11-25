d0e6cc5d173f9c5f78daa595c539430766901567
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2023-11-26 08:47:37 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,137 +1,236 @@
+$(document).ready(function () {
+    // 사이드바와 콘텐츠에 'active' 클래스 추가하여 시작 시 사이드바를 활성화
+    $('#sidebar, #content').addClass('active');
+    var sidebarWidth = $('#sidebar').width();
+    $('#content').css('margin-left', sidebarWidth + 'px');
+    $('#sidebarCollapse').css('left', sidebarWidth + 'px');
+    $('#toggleIcon').removeClass('fa-arrow-right').addClass('fa-arrow-left');
 
-var deletedStudents = [];
+    // Toggle sidebar
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        var sidebarWidth = $('#sidebar').width();
+        if ($('#sidebar').hasClass('active')) {
+            $('#content').css('margin-left', sidebarWidth + 'px');
+            $('#sidebarCollapse').css('left', sidebarWidth + 'px');
+            $('#toggleIcon').removeClass('fa-arrow-right').addClass('fa-arrow-left');
+        } else {
+            $('#content').css('margin-left', '0');
+            $('#sidebarCollapse').css('left', '0');
+            $('#toggleIcon').removeClass('fa-arrow-left').addClass('fa-arrow-right');
+        }
+    });
 
-function addStudent() {
-    var studentNameInput = document.getElementById("studentName");
-    var studentName = studentNameInput.value;
+    // File input change event
+    document.getElementById('fileInput').addEventListener('change', function (event) {
+        Array.from(event.target.files).forEach(function (file) {
+            fileData.push({ file: file, lines: [], originalContent: [], removedButtons: [] });
+            var fileButton = createFileButton(file);
+            document.getElementById('fileButtons').appendChild(fileButton);
+        });
+    });
 
-    if (studentName === "") {
-        return;
-    }
+    // Initialize sortable container
+    $("#fileContentsContainer").sortable();
+});
 
-    var studentList = document.getElementById("studentList");
-    var button = document.createElement("student-button");
-    button.textContent = studentName;
+// Global file data array
+var fileData = [];
+
+// Function to load a page into the main content
+function loadPage(url) {
+    $('#main-content').load(url);
+}
+
+// Create a button for each file
+function createFileButton(file) {
+    var button = document.createElement('button');
+    // 파일 이름에서 확장자 제거
+    var fileNameWithoutExtension = file.name.replace(/\.[^/.]+$/, "");
+    button.textContent = fileNameWithoutExtension;
+
     button.onclick = function () {
-        deleteStudent(button);
+        displayFileContent(file);
     };
-    studentList.appendChild(button);
 
-    studentNameInput.value = "";
+    // 버튼에 고유한 클래스 추가
+    button.classList.add('file-list-button');
+
+    return button;
 }
 
-function deleteStudent(button) {
-    var studentList = document.getElementById("studentList");
-    studentList.removeChild(button);
-    deletedStudents.push(button);
+// Display content of a file
+function displayFileContent(file) {
+    var fileIndex = fileData.findIndex(f => f.file === file);
+    var fileContentDiv = document.createElement('div');
+    fileContentDiv.className = 'file-content';
+
+    var fileDetails = createFileDetails(file, fileIndex);
+    fileContentDiv.appendChild(fileDetails);
+
+    var contentButtons = createContentButtons(file, fileIndex);
+    contentButtons.className = 'content-buttons';
+    fileContentDiv.appendChild(contentButtons);
+
+    $(contentButtons).sortable({
+        handle: '.drag-handle'
+    });
+
+    var iconButtonContainer = createIconButtonContainer(fileIndex, contentButtons);
+    fileContentDiv.appendChild(iconButtonContainer);
+
+    document.getElementById('fileContentsContainer').appendChild(fileContentDiv);
+}
+// 아이콘 버튼 컨테이너를 생성하고 반환하는 함수
+function createIconButtonContainer(fileIndex, contentButtons) {
+    var iconButtonContainer = document.createElement('div');
+    iconButtonContainer.className = 'icon-button-container';
+
+    // 내용 추가 버튼
+    var addButton = createIconButton('fa-plus', '내용 추가');
+    addButton.onclick = function () {
+        addContent(fileIndex, contentButtons);
+    };
+    iconButtonContainer.appendChild(addButton);
+
+    // 되돌리기 버튼
+    var undoButton = createIconButton('fa-undo-alt', '되돌리기');
+    undoButton.onclick = function () {
+        undoRemove(fileIndex, contentButtons);
+    };
+    iconButtonContainer.appendChild(undoButton);
+
+    // 내보내기 버튼
+    var exportButton = createIconButton('fa-file-export', '내보내기');
+    exportButton.onclick = function () {
+        exportToFile(contentButtons, fileData[fileIndex].file.name, fileIndex);
+    };
+    iconButtonContainer.appendChild(exportButton);
+
+    // 삭제 버튼
+    var deleteButton = createIconButton('fa-trash', '이 복사본 삭제');
+    deleteButton.onclick = function () {
+        // `deleteButton`의 상위 요소를 찾아서 삭제
+        var fileContentDiv = this.closest('.file-content');
+        if (fileContentDiv) {
+            fileContentDiv.remove();
+        }
+    };
+    iconButtonContainer.appendChild(deleteButton);
+
+    return iconButtonContainer;
 }
 
-function undoDelete() {
-    if (deletedStudents.length > 0) {
-        var studentList = document.getElementById("studentList");
-        var buttonToRestore = deletedStudents.pop();
-        studentList.appendChild(buttonToRestore);
+// Create an icon button
+function createIconButton(iconClass, title) {
+    var button = document.createElement('button');
+    button.classList.add('icon-button'); // 여기에 클래스 추가
+    var icon = document.createElement('i');
+    icon.className = 'fas ' + iconClass;
+    button.appendChild(icon);
+    button.title = title; // Tooltip
+    return button;
+}
+
+// Create file details display with text input for editing file name
+function createFileDetails(file, fileIndex) {
+    var fileInputDiv = document.createElement('div');
+    fileInputDiv.className = 'file-input'; 
+
+    var textInput = document.createElement('input');
+    textInput.type = 'text';
+    textInput.value = file.name; 
+    textInput.className = 'file-name-input'; 
+
+    textInput.addEventListener('change', function() {
+        if (fileData[fileIndex]) {
+            fileData[fileIndex].customFileName = this.value;
+        }
+    });
+
+    fileInputDiv.appendChild(textInput);
+    return fileInputDiv;
+}
+// Create buttons for content manipulation
+function createContentButtons(file, fileIndex) {
+    var contentButtons = document.createElement('div');
+    contentButtons.style.display = 'flex';
+    contentButtons.style.flexDirection = 'row'; // 수평 배열을 위해 row로 설정
+    contentButtons.style.flexWrap = 'wrap'; // 내용이 많을 경우 다음 줄로 넘김
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var lines = e.target.result.split('\n');
+        fileData[fileIndex].originalContent = lines.slice();
+        lines.forEach(function (line) {
+            var lineContainer = createLineContainer(line, fileIndex);
+            contentButtons.appendChild(lineContainer);
+        });
+    };
+    reader.readAsText(file);
+    return contentButtons;
+}
+
+// Create a container for each line of the file
+function createLineContainer(line, fileIndex) {
+    var lineContainer = document.createElement('div');
+    lineContainer.style.display = 'flex'; // flex 레이아웃 사용
+
+    var dragHandle = document.createElement('span');
+    dragHandle.textContent = '☰';
+    dragHandle.className = 'drag-handle';
+
+    var button = document.createElement('button');
+    button.textContent = line;
+    button.className = 'line-button'; // CSS 클래스 추가
+    button.onclick = function () {
+        fileData[fileIndex].removedButtons.push({ element: lineContainer, index: fileData[fileIndex].originalContent.indexOf(line) });
+        lineContainer.remove();
+    };
+
+    lineContainer.appendChild(dragHandle);
+    lineContainer.appendChild(button);
+    return lineContainer;
+}
+
+// Undo the last remove action
+function undoRemove(fileIndex, contentButtons) {
+    var lastRemoved = fileData[fileIndex].removedButtons.pop();
+    if (lastRemoved) {
+        var lineContainer = lastRemoved.element;
+        contentButtons.appendChild(lineContainer);
     }
 }
 
-function loadFile() {
-    initialize(); // 초기화 동작 추가
+// Export modified content to a file
+function exportToFile(contentButtons, defaultFileName, fileIndex) {
+    var customFileName = fileData[fileIndex].customFileName || defaultFileName;
 
-    var fileInput = document.getElementById("fileInput");
-    var file = fileInput.files[0];
-    if (file) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            var names = e.target.result.split('\n');
-            var gameDescription = document.getElementById("gameDescription");
-
-            if (names.length > 0) {
-                // Use the file name as the game description (removing the date part)
-                var fileName = file.name.replace(/_\d{4}-\d{2}-\d{2}/, "").replace(".txt", "").trim();
-                gameDescription.value = fileName;
-            }
-
-            names.forEach(function(name) {
-                if (name.trim() !== "") {
-                    var studentList = document.getElementById("studentList");
-                    var button = document.createElement("student-button");
-                    button.textContent = name.trim();
-                    button.onclick = function () {
-                        deleteStudent(button);
-                    };
-                    studentList.appendChild(button);
-                }
-            });
-        };
-        reader.readAsText(file);
-    }
-}
-
-function exportToTextFile() {
-    var gameDescription = document.getElementById("gameDescription").value;
-    var studentList = document.getElementById("studentList");
-    var buttons = studentList.getElementsByTagName("student-button");
-    var studentNames = [];
-    for (var i = 0; i < buttons.length; i++) {
-        studentNames.push(buttons[i].textContent);
+    // 파일 이름에 확장자가 없으면 '.txt'를 추가
+    if (!customFileName.endsWith('.txt')) {
+        customFileName += '.txt';
     }
 
-    var currentDate = new Date();
-    var year = currentDate.getFullYear();
-    var month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    var day = String(currentDate.getDate()).padStart(2, '0');
-    var dateString = year + '-' + month + '-' + day;
-
-    var text = studentNames.join("\n");
-    var blob = new Blob([text], { type: "text/plain" });
-    var a = document.createElement("a");
-    a.href = URL.createObjectURL(blob);
-    a.download = gameDescription + "_" + dateString + ".txt"; // 한국 시간에 맞게 파일 이름 생성
-    document.body.appendChild(a);
+    var lines = [];
+    for (var i = 0; i < contentButtons.children.length; i++) {
+        var lineContainer = contentButtons.children[i];
+        lines.push(lineContainer.querySelector('button').textContent);
+    }
+    var blob = new Blob([lines.join('\n')], { type: 'text/plain' });
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = customFileName;
     a.click();
-    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
 }
 
-function initialize() {
-    // 초기화 동작 추가
-    var gameDescription = document.getElementById("gameDescription");
-    gameDescription.value = "";
-    var studentList = document.getElementById("studentList");
-    studentList.innerHTML = "";
-}
-
-function updateTitle() {
-    var gameDescription = document.getElementById("gameDescription").value;
-    document.title = gameDescription;
-}
-
-function openNewTab() {
-    // 현재 페이지 URL을 가져와서 새 탭에서 열기
-    var currentURL = window.location.href;
-    window.open(currentURL, '_blank');
-}
-
-function downloadExampleFile() {
-    var exampleStudents = [
-        "김연아",
-        "제이홉",
-        "박지성",
-        "손흥민",
-        "홍승일",
-        "방탄",
-        "박보검",
-        "아이브",
-        "이유진",
-        "장원영",
-        // ... 다른 학생 이름들 추가 가능
-    ];
-
-    var text = exampleStudents.join("\n");
-    var blob = new Blob([text], { type: "text/plain" });
-    var a = document.createElement("a");
-    a.href = URL.createObjectURL(blob);
-    a.download = "example_students.txt"; 
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+// Add new content to a file
+function addContent(fileIndex, contentButtons) {
+    var newContent = prompt('추가할 내용을 입력하세요:');
+    if (newContent) {
+        var lineContainer = createLineContainer(newContent, fileIndex);
+        contentButtons.appendChild(lineContainer);
+        fileData[fileIndex].originalContent.push(newContent);
+    }
 }
