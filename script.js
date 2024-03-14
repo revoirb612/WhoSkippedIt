d698bb380bfbed88fc09d43ccc51cc8a6b5a0460
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2024-03-14 22:10:01 +0900

    Update script.js
    사이드바 토글 함수 개선

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 $(document).ready(function () {
-    // 사이드바와 콘텐츠에 'active' 클래스 추가하여 시작 시 사이드바를 활성화
-    $('#sidebar, #content').addClass('active');
-    var sidebarWidth = $('#sidebar').width();
-    $('#content').css('margin-left', sidebarWidth + 'px');
-    $('#sidebarCollapse').css('left', sidebarWidth + 37 + 'px');
-    $('#toggleIcon').removeClass('fa-arrow-right').addClass('fa-arrow-left');
+    toggleSidebar();
 
-    // Toggle sidebar
     $('#sidebarCollapse').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
-        var sidebarWidth = $('#sidebar').width();
-        if ($('#sidebar').hasClass('active')) {
-            $('#content').css('margin-left', sidebarWidth + 'px');
-            $('#sidebarCollapse').css('left', sidebarWidth + 37 + 'px');
-            $('#toggleIcon').removeClass('fa-arrow-right').addClass('fa-arrow-left');
-        } else {
-            $('#content').css('margin-left', '0');
-            $('#sidebarCollapse').css('left', '0');
-            $('#toggleIcon').removeClass('fa-arrow-left').addClass('fa-arrow-right');
-        }
+        toggleSidebar();
     });
 
     // File input change event
@@ -36,6 +20,25 @@ $(document).ready(function () {
     // Check if fileContentsContainer is empty and display a message if it is
     checkFileContentsContainer();
 });
+
+function toggleSidebar() {
+    var sidebarWidth = $('#sidebar').width();
+
+    // 사이드바와 콘텐츠의 'active' 클래스 토글
+    $('#sidebar, #content').toggleClass('active');
+
+    // 사이드바의 너비에 따라 콘텐츠와 버튼의 위치 조절
+    if ($('#sidebar').hasClass('active')) {
+        $('#content').css('margin-left', sidebarWidth + 'px');
+        $('#sidebarCollapse').css('left', sidebarWidth + 37 + 'px');
+        $('#toggleIcon').removeClass('fa-arrow-right').addClass('fa-arrow-left');
+    } else {
+        $('#content').css('margin-left', '0');
+        $('#sidebarCollapse').css('left', '0');
+        $('#toggleIcon').removeClass('fa-arrow-left').addClass('fa-arrow-right');
+        window.uploadData();  // 사이드바 비활성화 시 데이터 업로드
+    }
+}
 
 // Global file data array
 var fileData = [];
