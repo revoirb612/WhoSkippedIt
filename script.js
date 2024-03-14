0820c6e7cf2a57b602987448e15b8be124386242
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2024-03-14 22:50:11 +0900

    Update script.js
    코드 가독성 향상

--- script.js
+++ script.js
@@ -109,35 +109,7 @@ function displayFileContent(file) {
     checkFileContentsContainer(); // Check and update message after adding content
 }
 
-function deleteFileContent() {
-    var fileContentDiv = this.closest('.file-content');
-    if (fileContentDiv) {
-        fileContentDiv.remove();
-    }
-    checkFileContentsContainer();
-}
 
-function createIconButtonContainer(fileIndex, contentButtons) {
-    var iconButtonContainer = document.createElement('div');
-    iconButtonContainer.className = 'icon-button-container';
-
-    // 버튼 정보 및 클릭 이벤트 핸들러를 배열로 정의
-    var buttonsInfo = [
-        {icon: 'fa-plus', label: '내용 추가', onClick: () => addContent(fileIndex, contentButtons)},
-        {icon: 'fa-undo-alt', label: '되돌리기', onClick: () => undoRemove(fileIndex, contentButtons)},
-        {icon: 'fa-file-export', label: '내보내기', onClick: () => exportToFile(contentButtons, fileData[fileIndex].file.name, fileIndex)},
-        {icon: 'fa-trash', label: '이 복사본 삭제', onClick: deleteFileContent}
-    ];
-
-    // 버튼 정보 배열을 순회하며 각 버튼을 생성하고, 이벤트 핸들러를 바인딩
-    buttonsInfo.forEach(function(buttonInfo) {
-        var button = createIconButton(buttonInfo.icon, buttonInfo.label);
-        button.onclick = buttonInfo.onClick;
-        iconButtonContainer.appendChild(button);
-    });
-
-    return iconButtonContainer;
-}
 
 // Create an icon button
 function createIconButton(iconClass, title) {
@@ -212,7 +184,15 @@ function createLineContainer(line, fileIndex) {
     return lineContainer;
 }
 
-// Undo the last remove action
+function addContent(fileIndex, contentButtons) {
+    var newContent = prompt('추가할 내용을 입력하세요:');
+    if (newContent) {
+        var lineContainer = createLineContainer(newContent, fileIndex);
+        contentButtons.appendChild(lineContainer);
+        fileData[fileIndex].originalContent.push(newContent);
+    }
+}
+
 function undoRemove(fileIndex, contentButtons) {
     var lastRemoved = fileData[fileIndex].removedButtons.pop();
     if (lastRemoved) {
@@ -221,7 +201,6 @@ function undoRemove(fileIndex, contentButtons) {
     }
 }
 
-// Export modified content to a file
 function exportToFile(contentButtons, defaultFileName, fileIndex) {
     var customFileName = fileData[fileIndex].customFileName || defaultFileName;
 
@@ -244,14 +223,34 @@ function exportToFile(contentButtons, defaultFileName, fileIndex) {
     URL.revokeObjectURL(url);
 }
 
-// Add new content to a file
-function addContent(fileIndex, contentButtons) {
-    var newContent = prompt('추가할 내용을 입력하세요:');
-    if (newContent) {
-        var lineContainer = createLineContainer(newContent, fileIndex);
-        contentButtons.appendChild(lineContainer);
-        fileData[fileIndex].originalContent.push(newContent);
+function deleteFileContent() {
+    var fileContentDiv = this.closest('.file-content');
+    if (fileContentDiv) {
+        fileContentDiv.remove();
     }
+    checkFileContentsContainer();
+}
+
+function createIconButtonContainer(fileIndex, contentButtons) {
+    var iconButtonContainer = document.createElement('div');
+    iconButtonContainer.className = 'icon-button-container';
+
+    // 버튼 정보 및 클릭 이벤트 핸들러를 배열로 정의
+    var buttonsInfo = [
+        {icon: 'fa-plus', label: '내용 추가', onClick: () => addContent(fileIndex, contentButtons)},
+        {icon: 'fa-undo-alt', label: '되돌리기', onClick: () => undoRemove(fileIndex, contentButtons)},
+        {icon: 'fa-file-export', label: '내보내기', onClick: () => exportToFile(contentButtons, fileData[fileIndex].file.name, fileIndex)},
+        {icon: 'fa-trash', label: '이 복사본 삭제', onClick: deleteFileContent}
+    ];
+
+    // 버튼 정보 배열을 순회하며 각 버튼을 생성하고, 이벤트 핸들러를 바인딩
+    buttonsInfo.forEach(function(buttonInfo) {
+        var button = createIconButton(buttonInfo.icon, buttonInfo.label);
+        button.onclick = buttonInfo.onClick;
+        iconButtonContainer.appendChild(button);
+    });
+
+    return iconButtonContainer;
 }
 
 function downloadFile(filename, content) {
