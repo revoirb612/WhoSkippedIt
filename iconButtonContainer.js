d11cfd43f28adb2f2a9a0fe378acbac9878470c9
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2024-03-15 23:12:35 +0900

    Update iconButtonContainer.js

--- iconButtonContainer.js
+++ iconButtonContainer.js
@@ -1,1 +1,79 @@
+function addContent(fileIndex, contentButtons) {
+    var newContent = prompt('추가할 내용을 입력하세요:');
+    if (newContent) {
+        var lineContainer = createLineContainer(newContent, fileIndex);
+        contentButtons.appendChild(lineContainer);
+        fileData[fileIndex].originalContent.push(newContent);
+    }
+}
 
+function undoRemove(fileIndex, contentButtons) {
+    var lastRemoved = fileData[fileIndex].removedButtons.pop();
+    if (lastRemoved) {
+        var lineContainer = lastRemoved.element;
+        contentButtons.appendChild(lineContainer);
+    }
+}
+
+function exportToFile(contentButtons, defaultFileName, fileIndex) {
+    var customFileName = fileData[fileIndex].customFileName || defaultFileName;
+
+    // 파일 이름에 확장자가 없으면 '.txt'를 추가
+    if (!customFileName.endsWith('.txt')) {
+        customFileName += '.txt';
+    }
+
+    var lines = [];
+    for (var i = 0; i < contentButtons.children.length; i++) {
+        var lineContainer = contentButtons.children[i];
+        lines.push(lineContainer.querySelector('button').textContent);
+    }
+    var blob = new Blob([lines.join('\n')], { type: 'text/plain' });
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = customFileName;
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+function deleteFileContent() {
+    var fileContentDiv = this.closest('.file-content');
+    if (fileContentDiv) {
+        fileContentDiv.remove();
+    }
+    checkFileContentsContainer();
+}
+
+function createIconButtonContainer(fileIndex, contentButtons) {
+    var iconButtonContainer = document.createElement('div');
+    iconButtonContainer.className = 'icon-button-container';
+
+    // 버튼 정보 및 클릭 이벤트 핸들러를 배열로 정의
+    var buttonsInfo = [
+        {icon: 'fa-plus', label: '내용 추가', onClick: () => addContent(fileIndex, contentButtons)},
+        {icon: 'fa-undo-alt', label: '되돌리기', onClick: () => undoRemove(fileIndex, contentButtons)},
+        {icon: 'fa-file-export', label: '내보내기', onClick: () => exportToFile(contentButtons, fileData[fileIndex].file.name, fileIndex)},
+        {icon: 'fa-trash', label: '이 복사본 삭제', onClick: deleteFileContent}
+    ];
+
+    // Create an icon button
+    function createIconButton(iconClass, title) {
+        var button = document.createElement('button');
+        button.classList.add('icon-button'); // 여기에 클래스 추가
+        var icon = document.createElement('i');
+        icon.className = 'fas ' + iconClass;
+        button.appendChild(icon);
+        button.title = title; // Tooltip
+        return button;
+    }
+    
+    // 버튼 정보 배열을 순회하며 각 버튼을 생성하고, 이벤트 핸들러를 바인딩
+    buttonsInfo.forEach(function(buttonInfo) {
+        var button = createIconButton(buttonInfo.icon, buttonInfo.label);
+        button.onclick = buttonInfo.onClick;
+        iconButtonContainer.appendChild(button);
+    });
+
+    return iconButtonContainer;
+}
