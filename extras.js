7d2820547ce6594a0644b755ab1138e0bb7116aa
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2024-03-15 23:40:31 +0900

    Update extras.js

--- extras.js
+++ extras.js
@@ -9,3 +9,18 @@ function downloadFile(filename, content) {
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
 }
+
+// Function to check if fileContentsContainer is empty and display a message
+function checkFileContentsContainer() {
+    var container = $("#fileContentsContainer");
+    if (container.children().length === 0) {
+        var emptyMessage = $('<div/>', {
+            id: 'emptyContainerMessage',
+            text: '누가 누가 안 했나 for Web'
+        });
+
+        container.append(emptyMessage);
+    } else {
+        $('#emptyContainerMessage').remove();
+    }
+}
